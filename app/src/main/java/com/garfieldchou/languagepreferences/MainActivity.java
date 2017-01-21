ee3e9c758f7feb64be13cafe9c665f7e0e82a1d3
shaudai <shaudai0402@hotmail.com>
2017-01-21 23:38:15 +0800

    Create an AlertDialog for language selection

--- app/src/main/java/com/garfieldchou/languagepreferences/MainActivity.java
+++ app/src/main/java/com/garfieldchou/languagepreferences/MainActivity.java
@@ -1,7 +1,10 @@
 package com.garfieldchou.languagepreferences;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_btn_speak_now)
+                .setTitle("Choose a language")
+                .setMessage("Which language would you like?")
+                .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Select English!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("SPANISH", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Select SPANISH!", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
     }
 }
