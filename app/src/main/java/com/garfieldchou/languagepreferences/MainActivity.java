171a2500ec788aaa9074ff46e3c3149785668a77
shaudai <shaudai0402@hotmail.com>
2017-01-21 23:46:59 +0800

    Update TextView according to selection

--- app/src/main/java/com/garfieldchou/languagepreferences/MainActivity.java
+++ app/src/main/java/com/garfieldchou/languagepreferences/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView langTextView = (TextView) findViewById(R.id.langTextView);
+
         new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_btn_speak_now)
                 .setTitle("Choose a language")
                 .setMessage("Which language would you like?")
@@ -20,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Toast.makeText(MainActivity.this, "Select English!", Toast.LENGTH_SHORT).show();
+                        langTextView.setText("English");
                     }
                 })
                 .setNegativeButton("SPANISH", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Toast.makeText(MainActivity.this, "Select SPANISH!", Toast.LENGTH_SHORT).show();
+                        langTextView.setText("Spanish");
                     }
                 }).show();
     }
